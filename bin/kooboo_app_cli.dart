95b0d7b40483592b1b7ca4226ba0c30fc366342c
何宗杰 <zongjie@kooboo.net>
2022-03-03 15:54:42 +0800

    init project

---
+++ bin/kooboo_app_cli.dart
@@ -0,0 +1,5 @@
+import 'package:kooboo_app_cli/kooboo_app_cli.dart' as kooboo_app_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${kooboo_app_cli.calculate()}!');
+}
---
+++ test/kooboo_app_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:kooboo_app_cli/kooboo_app_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
